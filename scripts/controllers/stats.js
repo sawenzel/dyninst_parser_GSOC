8989d2832dbbbd5c190836231ab94be26bb1fcac
Alin Mindroc <mindroc.alin@gmail.com>
2015-07-08 23:27:43 +0300

    added startswith polyfill for safari

--- scripts/controllers/stats.js
+++ scripts/controllers/stats.js
@@ -26,6 +26,15 @@
  		'other' : ['other']
  	};
 
+ 	//we don't have startsWith on safari
+ 	if (!String.prototype.startsWith) {
+ 		String.prototype.startsWith = function(searchString, position) {
+ 			position = position || 0;
+ 			return this.indexOf(searchString, position) === position;
+ 		};
+ 	}
+
+
  	$scope.exit = function() {
  		$modalInstance.close();
  	};
