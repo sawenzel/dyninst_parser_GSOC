8875d36f5711a23bc1524110353ac91883d4c374
Alin Mindroc <mindroc.alin@gmail.com>
2015-07-11 00:54:13 +0300

    archive parser returns valid json

--- archiveparser.cc
+++ archiveparser.cc
@@ -13,27 +13,26 @@ using namespace InstructionAPI;
 
 int main(int argc, char **argv){
 	Instruction::Ptr instr;
-
 	Archive *ar = NULL;
-
 	vector<Symtab *> symTabs;
+
+	stringstream outstream;
 
 	Archive::openArchive(ar, argv[1]);
 	if(ar == NULL){
-		//TODO: error message here
-		cout << "empty archive" << endl;
+		outstream << "{empty archive}";
 		return -1;
 	}
+
 	ar->getAllMembers(symTabs);
 
-	cout << symTabs.size() << endl;
+	outstream << "{";
 
-	map <string, int> fmap;
+	auto sit = symTabs.begin();
+	for(; sit != symTabs.end(); ++sit){
+		int func_count = 0;
 
-	auto fit = symTabs.begin();
-	for(int i=0; fit != symTabs.end(); ++fit, i++){
-		Symtab *s = *fit;
-		cout << "file: " << s->name() << endl;
+		Symtab *s = *sit;
 		SymtabCodeSource *sts = new SymtabCodeSource(s);
 
 		CodeObject *co = new CodeObject(sts);
@@ -43,33 +42,36 @@ int main(int argc, char **argv){
 		vector<CodeRegion *> regions = co->cs()->regions();
 
 		if(regions.size() == 0){
-			cout << "no code regions" << endl;
+			//no code regions
 			continue;
 		}
 
 		const CodeObject::funclist &all = co->funcs();
 		if(all.size() == 0){
-			cout << "no functions" << endl;
+			//no functions
 			continue;
 		}
-		cout << all.size();
+
+		outstream << "\"" << s->name() << "\":[";
 
 		Address crtAddr;
 
-		auto fitf = all.begin();
-		ParseAPI::Function *f = *fitf;
+		auto fit = all.begin();
+		ParseAPI::Function *f = *fit;
 		InstructionDecoder decoder(f->isrc()->getPtrToInstruction(f->addr()),
 				InstructionDecoder::maxInstructionLength,
 				f->isrc()->getArch());
 
 		//iterate the ContainerWrapper through all the functions
-		unsigned int findex = 0;
-		for(;fitf != all.end(); ++fitf){
-			ParseAPI::Function *f = *fitf;
+		for(;fit != all.end(); ++fit){
+			if(func_count != 0)
+				outstream << ",";
+			func_count++;
+			ParseAPI::Function *f = *fit;
 			//if the function exists, don't output it
 
 			//if(strcmp(f->name().c_str(), "main") == 0){
-			cout << endl << "\"" << f->name() << "\" : \"[" << endl;
+			outstream << "{\"" << f->name() << "\" : [";
 			//get address of entry point for current function
 			//Address crtAddr = f->entry()->start();
 			crtAddr = f->addr();
@@ -83,23 +85,53 @@ int main(int argc, char **argv){
 
 			while(crtAddr < lastAddr){
 				if(instr_count != 0)
-					cout << ",";
+					outstream << ",";
 				//decode current instruction
-				cout << f->isrc()->isValidAddress(crtAddr);
-
 				instr = decoder.decode((unsigned char *)f->isrc()->getPtrToInstruction(crtAddr));
 
-				cout << endl << "{\\\"address\\\":\\\"" << hex << crtAddr << "\\\", ";
-				cout << "\\\"name\\\": \\\"" << instr->format() << "\\\"";
+				outstream << "{\"address\":\"" << hex << crtAddr << "\", ";
+				outstream << "\"name\": \"" << instr->format() << "\"";
+				//pentru instructiuni de tip call afisam adresa destinatie
+
+				if(instr->getCategory() == 0 && instr->size() == 5){
+					//aflam target-ul control flow-ului
+					Expression::Ptr expr = instr->getControlFlowTarget();
+					if(expr){
+						std::vector<Expression::Ptr> children1;
+						std::vector<Expression::Ptr> children2;
+						expr->getChildren(children1);
+						if(children1.size() > 1)
+							children1[1]->getChildren(children2);
+
+						//bind the RIP register value inside of the instruction
+						//to that of the current address + size
+						if(children1.size() > 1 && children2.size() > 1){
+							expr->bind(children1[1].get(), Result(u32, crtAddr + instr->size()));
+							//outstream << " : " << expr->eval().format();
+
+							//get the destination function
+							ParseAPI::Function *dest = co->findFuncByEntry(f->region(), expr->eval().convert<unsigned long int>());
+							//pt cazurile in care al doilea node al AST-ului nu era RIP
+							if(dest)
+								outstream << ", \"dest\": \"" << dest->name() << "\"";
+						}
+					}
+				}
+				//go to the address of the next instruction by adding the
+				//size of the current instruction to the current address
 
 				crtAddr += instr->size();
-				cout << "}";
+				outstream << "}";
 				instr_count++;
 			};
-			//decode instructions until return type instruction is found
-			//i.e. category = 1
-			cout << "]\"," << endl;
-			findex++;
+			outstream << "]}";
 		}
+		outstream << "],";
+		}
+
+		string resp = outstream.str();
+		resp.pop_back();
+		resp.append("}");
+
+		cout << resp;
 	}
-}
